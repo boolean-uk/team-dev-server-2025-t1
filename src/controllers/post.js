a3f334a503c15db57fd7fa3b62f297398dd6996e
Matilda Sönnergaard <matilda.sonnergaard@se.experis.com>
2025-02-28 09:44:30 +0100

    Create post model and controller

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -1,28 +1,41 @@
+import Post from '../domain/post.js'
+import User from '../domain/user.js'
 import { sendDataResponse } from '../utils/responses.js'
 
 export const create = async (req, res) => {
-  const { content } = req.body
+  const timestamp = new Date()
+  const postToCreate = await Post.fromJson({
+    ...req.body,
+    createdAt: timestamp,
+    updatedAt: timestamp
+  })
 
-  if (!content) {
-    return sendDataResponse(res, 400, { content: 'Must provide content' })
-  }
+  if (!User.findById(postToCreate.userId))
+    return sendDataResponse(res, 400, { content: 'No user with ID' })
 
-  return sendDataResponse(res, 201, { post: { id: 1, content } })
+  // if (!postToCreate.content) {
+  //   return sendDataResponse(res, 400, { content: 'Must provide content' })
+  // }
+
+  const createdPost = await postToCreate.save()
+
+  return sendDataResponse(res, 201, { post: createdPost })
+}
+
+export const getAllByUserId = async (req, res) => {
+  const foundPosts = (await Post.findByUserId(req.body)).map((post) => {
+    return { ...post.toJson().post }
+  })
+  return sendDataResponse(res, 200, {
+    posts: foundPosts
+  })
 }
 
 export const getAll = async (req, res) => {
+  const foundPosts = (await Post.findAll()).map((post) => {
+    return { ...post.toJson().post }
+  })
   return sendDataResponse(res, 200, {
-    posts: [
-      {
-        id: 1,
-        content: 'Hello world!',
-        author: { ...req.user }
-      },
-      {
-        id: 2,
-        content: 'Hello from the void!',
-        author: { ...req.user }
-      }
-    ]
+    posts: foundPosts
   })
 }
